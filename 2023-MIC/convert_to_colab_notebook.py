98329c15410562888ef9a5970869a8fbba619aba
Georg <gschra2@stanford.edu>
2023-07-01 14:14:16 -0700

    convert local image path to github url to get images also in colab

--- 2023-MIC/convert_to_colab_notebook.py
+++ 2023-MIC/convert_to_colab_notebook.py
@@ -1,5 +1,7 @@
-import jupytext
 from pathlib import Path
+import jupytext
+
+repo_path = 'https://github.com/gschramm/parallelproj-examples/blob/main/2023-MIC/'
 
 notebook_path = Path('01_poisson_projection_varnet.py')
 
@@ -9,7 +11,17 @@ with open('00_colab_setup.py', 'r') as f:
 with open(notebook_path, 'r') as f:
     notebook_str = f.read()
 
+# replace local image path with raw github link, such that
+# images are loaded in colab
+notebook_str = notebook_str.replace(
+    '![](figs/',
+    '![](https://github.com/gschramm/parallelproj-examples/blob/main/2023-MIC/figs/'
+)
+notebook_str = notebook_str.replace('.png)', '.png?raw=True)')
+
+# join colab setup cells and notebook cells
 notebook = jupytext.reads(header_str + '\n' + notebook_str)
 
+# write colab notebook to file
 jupytext.write(notebook,
                notebook_path.parent / f'{notebook_path.stem}_colab.ipynb')
