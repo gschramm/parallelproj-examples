a377b1616268ea40a10bfc456d3eec9f4c44864a
Georg <gschra2@stanford.edu>
2023-07-31 11:43:15 -0700

    convert example to "notebook"

--- 2023-MIC/00_fwd_back_projection.py
+++ 2023-MIC/00_fwd_back_projection.py
@@ -1,24 +1,29 @@
+# # Forward and back projection tutorial
+
+# +
 from __future__ import annotations
-
 import array_api_compat.torch as xp
-from array_api_compat import to_device
-import matplotlib.pyplot as plt
-
-from backend import ParallelViewProjector3D
-
 dev = 'cuda'
 
-#----------------------------------------------------------------
-
 print(f'running on {dev} device using {xp.__name__}')
+# -
 
-# shape of the 2D image
+from array_api_compat import to_device
+import matplotlib.pyplot as plt
+from backend import ParallelViewProjector3D
+
+# ### Image parameters
+
+# shape of our 3D image
 image_shape = (128, 128, 8)
-# voxel size
+# voxel size of our 3D image
 voxel_size = (2., 2., 4.)
 # world coordinates of the [0, 0] pixel
 image_origin = (-127., -127., -14.)
 
+# ### Projector parameters
+
+# +
 # radial positions of the projection lines
 radial_positions = to_device(xp.linspace(-128, 128, 200), dev)
 # projection angles
@@ -28,6 +33,7 @@ radius = 200.
 # axial coordinates of the projection "rings"
 ring_positions = to_device(xp.linspace(-14, 14, 8), dev)
 
+# create a 3D parallel view projector
 proj3d = ParallelViewProjector3D(image_shape,
                                  radial_positions,
                                  view_angles,
@@ -36,16 +42,23 @@ proj3d = ParallelViewProjector3D(image_shape,
                                  voxel_size,
                                  ring_positions,
                                  max_ring_diff=5)
+# -
+
+# ### Create a simple 3D test image
 
 img = to_device(xp.zeros(proj3d.in_shape, dtype=xp.float32), dev)
 img[32:96, 32:64, 1:-1] = 1.
 img[48:64, 48:54, 3:-3] = 2.
 
+# ### Forward and back project the test image
+
 img_fwd = proj3d(img)
 img_fwd_back = proj3d.adjoint(img_fwd)
 
-#----------------------------------------------------------------------------
-# show the geometry of the projector and the projections
+# ### Show the image, the forward projection and the backprojection
+
+# +
+### note: we only show the "direct" planes of the forward projection
 
 fig, ax = plt.subplots(3, 8, figsize=(16, 6))
 for i in range(8):
@@ -62,4 +75,4 @@ for i in range(8):
 for axx in ax.ravel():
     axx.set_axis_off()
 fig.tight_layout()
-fig.show()
+plt.show()
